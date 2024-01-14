da673d9359996cfa3e166b2c1cc3a76d93ae4b5a
Joni Toyryla <info@jonitoyryla.eu>
2024-01-14 18:41:49 +0200

    fix(typo): wrong url

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -42,7 +42,7 @@ export default function Footer() {
                     <Link href="/api/taxomageias/first/taxons" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">API</Link>
                   </li>
                   <li className="mb-1">
-                    <Link href="http://blog.taxomageia.com" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Blog</Link>
+                    <Link href="http://blog.taxomageia.pro" className="text-gray-400 hover:text-gray-100 transition duration-150 ease-in-out">Blog</Link>
                   </li>
                 </ul>
               </div>
