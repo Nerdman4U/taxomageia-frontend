d49fd2f3b397096819db2197b5928c49d7b52ab0
Joni Toyryla <info@jonitoyryla.eu>
2024-01-16 10:14:11 +0200

    fix(link): wrong link

--- components/hero.tsx
+++ components/hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
                 <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#creatures">Creatures</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/doc.html">Learn more</a>
+                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/documentation.html">Learn more</a>
               </div>
             </div>
           </div>
