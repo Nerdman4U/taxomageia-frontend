1e51b5196744a749990c017f69d34078fe982191
Joni Toyryla <info@jonitoyryla.eu>
2024-01-16 08:48:49 +0200

    feat(versions): static versions page

--- app/versions/page.tsx
+++ app/versions/page.tsx
@@ -1,28 +1,44 @@
 import { useContext} from "react"
 import AppContext from '@/app/context/application.context'
 import { AppContextType, VersionInfoType } from "@/app/context/application.context"
+import versions from "../../app/versions/versions.json" assert { type: "json" }
 
 const Version = ({info}: {info: VersionInfoType}) => {
+  const next = info.next_en || []
+  const features = info.features_en || []
+  const problems = info.problems_en || []
   return (
     <div id="what-is-taxomageia" className="max-w-3xl mx-auto md:pb-16">
       <h3 className="h3 mb-3 text-gray-400">Server: {info.version}</h3>
-      <p>    
-        // version features, problems etc.
-      </p>
+      <div>    
+        <h3>Features</h3>
+        <ul>
+          { features.map((item) => <li>{item}</li>) }
+        </ul>
+        <h3>Known problems</h3>
+        <ul>
+          { problems.map((item) => <li>{item}</li>) }
+        </ul>
+        <h3>What next?</h3>
+        <ul>
+          { next.map((item) => <li>{item}</li>) }
+        </ul>
+      </div>
     </div>  
   )
 }
 
 const Versions = () => {
   // TODO: use context and read version info.
-  const value = useContext(AppContext)
-  console.log('value:', value)
+  // const value = useContext(AppContext)
+  const versionInfo = versions
+  // console.log('value:', value)
   // const clientVersion = value.clientVersion || "0.0.0"
   // const serverVersion = value.serverVersion || "0.0.0"
   // const clientVersionStr = value.clientVersionStr || "Client: v0.0.0"
   // const serverVersionStr = value.serverVersionStr || "Server: v0.0.0"
   // const versionInfo = value.versionInfo || []
-  // console.log(versionInfo)
+  console.log(versionInfo)
 
   return ( 
     <section id="versions">
@@ -39,7 +55,7 @@ const Versions = () => {
           </div>
    
           {/* Items */}
-          {/* { versionInfo?.map((info) => <Version key={info.version} info={info} />) } */}
+          { versionInfo?.map((info) => <Version key={info.version} info={info} />) }
 
         </div>
       </div>
