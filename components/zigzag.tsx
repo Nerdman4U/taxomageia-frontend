af85f44524e7231010b5b673c6e18a9c9415fa1f
Joni Toyryla <info@jonitoyryla.eu>
2024-01-21 18:36:31 +0200

    style(words): few words at front page

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -29,8 +29,8 @@ const Zigzag = ({ taxons, handleSelectRankClick, clicked }: { taxons: rank[], ha
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             {/* <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div> */}
-            <h1 className="h2 mb-4">Creatures ({ taxons.length })</h1>
-            <p className="text-xl text-gray-400">Click the name of the taxon to see more</p>
+            <h1 className="h2 mb-4">Creatures and monsters ({ taxons.length })</h1>
+            <p className="text-xl text-gray-400">Click and get fresh and free copy of monster JSON!</p>
           </div>
 
           {/* Items */}
