22de391c48eb82de070ba6c693ab0a07843725aa
Joni Toyryla <info@jonitoyryla.eu>
2024-02-20 12:27:43 +0200

    feat(diary): removed little bit of text

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -9,7 +9,7 @@ const Diary = () => {
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             {/* <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div> */}
             <h1 className="h2 mb-4">Diary</h1>
-            <p className="text-xl text-gray-400">I will add "daily" developer notes here. Just for to share with you.</p>
+            <p className="text-xl text-gray-400">I will add "daily" developer notes here.</p>
           </div>
 
           <div className="max-w-3xl mx-auto md:pb-16">
